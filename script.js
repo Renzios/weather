1d0ebd105d38308c513bfdbd1f58897d9fbbba64
Renzios <61353378+Renzios@users.noreply.github.com.>
2024-09-21 18:36:57 +0800

    feat: remove date

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const time = document.querySelector(".time");
-const date = document.querySelector(".date");
 
 function setDate() {
     const now = new Date();
     time.textContent = now.toLocaleTimeString();
-    date.textContent = now.toLocaleDateString();
 }
 
 setDate();
